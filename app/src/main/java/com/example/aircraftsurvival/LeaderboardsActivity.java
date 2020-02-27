b5284939b91d6db2c0e2a7ea4b23a1d7efe7442c
Logan11999 <32521796+Logan11999@users.noreply.github.com>
2020-02-27 10:47:16 -0600

    added empty activities for the game, leaderboards, and settings

---
+++ app/src/main/java/com/example/aircraftsurvival/LeaderboardsActivity.java
@@ -0,0 +1,14 @@
+package com.example.aircraftsurvival;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LeaderboardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboards);
+    }
+}
