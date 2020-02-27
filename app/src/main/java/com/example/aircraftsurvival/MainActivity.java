fd0751cb9bbfdb765de067a73e367329e7ad2002
Logan11999 <32521796+Logan11999@users.noreply.github.com>
2020-02-27 11:08:00 -0600

    Fixed typo in activity names

--- app/src/main/java/com/example/aircraftsurvival/MainActivity.java
+++ app/src/main/java/com/example/aircraftsurvival/MainActivity.java
@@ -16,25 +16,25 @@ public class MainActivity extends AppCompatActivity {
 
     //connects new game button to game activity
     public void newClick(View v){
-        Intent intent = new Intent(MainActivity.this, gameActivity.class);
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
         startActivity(intent);
     }
 
     //connects load game button to game activity
     public void loadlick(View v){
-        Intent intent = new Intent(MainActivity.this, gameActivity.class);
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
         startActivity(intent);
     }
 
     //connects leaderboards button to leaderboards activity
     public void leaderboardsClick(View v){
-        Intent intent = new Intent(MainActivity.this, leaderboardsActivity.class);
+        Intent intent = new Intent(MainActivity.this, LeaderboardsActivity.class);
         startActivity(intent);
     }
 
     //connects settings button to settings activity
     public void settingsClick(View v){
-        Intent intent = new Intent(MainActivity.this, settingsActivity.class);
+        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
         startActivity(intent);
     }
 }
