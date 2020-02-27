72d44a919bee07cd96e5154dcbbe65c10852c024
Logan11999 <32521796+Logan11999@users.noreply.github.com>
2020-02-27 10:42:42 -0600

    Added intents to start new activities on button click and connected buttons to them

--- app/src/main/java/com/example/aircraftsurvival/MainActivity.java
+++ app/src/main/java/com/example/aircraftsurvival/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.aircraftsurvival;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //connects new game button to game activity
+    public void newClick(View v){
+        Intent intent = new Intent(MainActivity.this, gameActivity.class);
+        startActivity(intent);
+    }
+
+    //connects load game button to game activity
+    public void loadlick(View v){
+        Intent intent = new Intent(MainActivity.this, gameActivity.class);
+        startActivity(intent);
+    }
+
+    //connects leaderboards button to leaderboards activity
+    public void leaderboardsClick(View v){
+        Intent intent = new Intent(MainActivity.this, leaderboardsActivity.class);
+        startActivity(intent);
+    }
+
+    //connects settings button to settings activity
+    public void settingsClick(View v){
+        Intent intent = new Intent(MainActivity.this, settingsActivity.class);
+        startActivity(intent);
     }
 }
