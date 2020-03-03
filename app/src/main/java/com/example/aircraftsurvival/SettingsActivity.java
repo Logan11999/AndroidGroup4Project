52657876b8c3f88fd8ff745396384a152a90032b
Logan11999 <32521796+Logan11999@users.noreply.github.com>
2020-03-02 23:20:23 -0600

    Added settings widgets and blank click methods

--- app/src/main/java/com/example/aircraftsurvival/SettingsActivity.java
+++ app/src/main/java/com/example/aircraftsurvival/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.aircraftsurvival;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+    //button functionalities might add more options later
+    public void musicClick(){
+    }
+    public void sfxClick(){
+    }
+    public void difficultyClick(){
     }
 }
