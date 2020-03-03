be572f438f7df053efd820edeb1684af64ac8602
Logan11999 <32521796+Logan11999@users.noreply.github.com>
2020-03-02 22:46:54 -0600

    added login dialog poppup and contents

--- app/src/main/java/com/example/aircraftsurvival/MainActivity.java
+++ app/src/main/java/com/example/aircraftsurvival/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.aircraftsurvival;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +16,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //connects to log in dialogue
+    public void loginClick(View v) {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.loginlayout);
+        EditText usernameET = (EditText) dialog.findViewById(R.id.usernameET);
+        EditText passwordET = (EditText) dialog.findViewById(R.id.passwordET);
+        Button loginBTN = (Button) dialog.findViewById(R.id.enterBTN);
+        Button setupBTN = (Button) dialog.findViewById(R.id.newUserBTN);
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
 
-    //connects to log in screen
-    //public void loginClick(View v){
-    //}
+    //will verify login possibly on backendless
+    public void existingLogin(){
+    }
+    //same for creating a user
+    public void newLogin(){
+    }
 
     //connects new game button to game activity
     public void newClick(View v){
